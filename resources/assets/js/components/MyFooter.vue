c26deaf43127c7b66db883fc0e12d7c3ec4fef43
taiki <buena960926@gmail.com>
2018-08-12 22:32:21 +0900

    Bulma,Vue.js導入

---
+++ resources/assets/js/components/MyFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <strong>GourSearch</strong> by <a href="https://kapi-travel.com">Taiki Shiino</a>. The source code is licensed
+                <a href="https://github.com/taikishiino">Github.com/taikishiino</a>
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "MyFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/MyHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+                <img src="" alt="GourSearch" width="112" height="28">
+            </a>
+
+            <div class="navbar-menu is-active">
+                <a class="navbar-item" href="">Home</a>
+                <a class="navbar-item" href="">About</a>
+            </div>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "MyHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
