5b8677e6616fbd43c0a7b2947381b6b5b8ed0290
taiki <buena960926@gmail.com>
2018-09-20 00:08:14 +0900

    index.blade.phpに#main、#sidebarレイアウトを実装

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<h1>TOP</h1>
+    <h1>TOP</h1>
+    <div id="contentWrapper">
+        <section id="sidebar" style="background: #000;">
+            sss
+        </section>
+        <section id="main" style="background: #00b89c;">
+            mmm
+        </section>
+    </div>
 @endsection
