41caf841aeecba6da2e617eebc976b722ada2230
taiki <buena960926@gmail.com>
2018-09-20 00:04:50 +0900

    bulmaから独自cssに移行（独自scssでのコーディングで開発するため）

--- resources/assets/js/components/MyHeader.vue
+++ resources/assets/js/components/MyHeader.vue
@@ -1,27 +1,35 @@
 <template>
-    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="/">
-                <img src="" alt="Gourmelinks" width="112" height="28">
-            </a>
+    <header>
+        <div class="hWarapper">
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="/">
+                        <img src="" alt="Gourmelinks" width="112" height="28">
+                    </a>
 
-            <div class="navbar-burger burger" data-target="navbarMenu" @click="showNav = !showNav" :class="{'is-active': showNav}">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </div>
-        </div>
-        <div class="navbar-menu" :class="{'is-active':showNav}">
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="field is-grouped">
-                        <a class="button is-primary" style="margin-right: 10px;">無料会員登録</a>
-                        <a class="button is-primary">ログイン</a>
+                    <div class="navbar-burger burger" data-target="navbarMenu" @click="showNav = !showNav" :class="{'is-active': showNav}">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
                     </div>
                 </div>
-            </div>
+                <div class="navbar-menu" :class="{'is-active':showNav}">
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="field is-grouped">
+                                <div class="control">
+                                    <a class="button is-primary">無料会員登録</a>
+                                </div>
+                                <div class="control">
+                                    <a class="button is-primary">ログイン</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
-    </nav>
+    </header>
 </template>
 
 <script>
@@ -34,5 +42,6 @@
 </script>
 
 <style scoped>
-
+    header, nav { background: #f2f2f2;}
+    .hWarapper { width: 1000px; margin: 0 auto;}
 </style>
